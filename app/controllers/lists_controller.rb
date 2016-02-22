9db647a74b320fe16871f2286767597bd0b0414b
Andy <abeaudoin2013@gmail.com>
2016-02-21 23:17:02 -0500

    added form for creating todo list items

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,9 +1,17 @@
 class ListsController < ApplicationController
   def index
   	@lists = List.all
+  	@list = List.new
   end
 
-  def new
-    @list = List.new
+  def create
+    @list = List.create(list_params)
+    redirect_to '/'
+  end
+
+  private
+
+  def list_params
+  	params.require(:list).permit(:title, :description, :progress)
   end
 end
